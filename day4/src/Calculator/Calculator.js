94cb35211acfa2772b0f3af6ec6b32e4122504fa
Nello Rizzo <NelloRizzo@users.noreply.github.com>
2024-10-10 13:45:15 +0200

    Add files via upload

--- day4/src/Calculator/Calculator.js
+++ day4/src/Calculator/Calculator.js
@@ -6,40 +6,39 @@ function Display({ value }) {
         <div className='display'>{value}</div>
     )
 }
-function Keyboard({ digitClick, zeroClicked, operatorClick }) {
+function Digit({ n, digitClick }) {
+    return (
+        <div className='button' onClick={() => digitClick(n)}>{n}</div>
+    )
+}
+function Keyboard(props) {
     return (
         <div className='keyboard'>
             <div className='row'>
             </div>
             <div className='row'>
-                <div className='button specials'>C</div>
-                <div className='button specials'>CE</div>
+                <div className='button specials' onClick={props.clickOnC}>C</div>
+                <div className='button specials' onClick={props.clickOnCE}>CE</div>
                 <div className='empty-space'></div>
-                <div className='button op' onClick={() => operatorClick('/')}>&divide;</div>
+                <div className='button op' onClick={() => props.operatorClick('/')}>&divide;</div>
             </div>
             <div className='row'>
-                <div className='button' onClick={() => digitClick(7)}>7</div>
-                <div className='button' onClick={() => digitClick(8)}>8</div>
-                <div className='button' onClick={() => digitClick(9)}>9</div>
-                <div className='button op' onClick={() => operatorClick('*')}>&times;</div>
+                {[7, 8, 9].map(v => <Digit n={v} digitClick={props.digitClick} />)}
+                <div className='button op' onClick={() => props.operatorClick('*')}>&times;</div>
             </div>
             <div className='row'>
-                <div className='button' onClick={() => digitClick(4)}>4</div>
-                <div className='button' onClick={() => digitClick(5)}>5</div>
-                <div className='button' onClick={() => digitClick(6)}>6</div>
-                <div className='button op' onClick={() => operatorClick('+')}>+</div>
+                {[4, 5, 6].map(v => <Digit n={v} digitClick={props.digitClick} />)}
+                <div className='button op' onClick={() => props.operatorClick('+')}>+</div>
             </div>
             <div className='row'>
-                <div className='button' onClick={() => digitClick(1)}>1</div>
-                <div className='button' onClick={() => digitClick(2)}>2</div>
-                <div className='button' onClick={() => digitClick(3)}>3</div>
-                <div className='button op' onClick={() => operatorClick('-')}>-</div>
+                {[1, 2, 3].map(v => <Digit n={v} digitClick={props.digitClick} />)}
+                <div className='button op' onClick={() => props.operatorClick('-')}>-</div>
             </div>
             <div className='row'>
-                <div className='button'></div>
-                <div className='button' onClick={zeroClicked}>0</div>
-                <div className='button'>,</div>
-                <div className='button op' onClick={() => operatorClick('=')}>=</div>
+                <div className='button' onClick={props.changeSignum}>&plusmn;</div>
+                <div className='button' onClick={props.zeroClicked}>0</div>
+                <div className='button' onClick={props.commaClicked}>,</div>
+                <div className='button op' onClick={() => props.operatorClick('=')}>=</div>
             </div>
         </div>
     )
@@ -67,6 +66,28 @@ export default function Calculator() {
             setMustClear(false)
         }
     }
+    function handleC() {
+        setDisplay('0.0')
+        setValue(0)
+        setOperator('=')
+        setMustClear(true)
+    }
+    function handleCE() {
+        setDisplay('0.0')
+        setMustClear(true)
+    }
+    function clickOnComma() {
+        let actual = mustClear ? '' : display
+        if (actual.indexOf('.') < 0) {
+            setDisplay(actual + '.')
+        }
+    }
+    function changeSignum() {
+        let actual = mustClear ? '' : display
+        if (actual.length > 0) {
+            setDisplay(-1 * actual)
+        }
+    }
     function executeOperation(op) {
         let actual = 1 * display
         switch (operator) {
@@ -84,9 +105,13 @@ export default function Calculator() {
         <div className='calc'>
             <Display value={display} />
             <Keyboard
+                clickOnC={handleC}
+                clickOnCE={handleCE}
                 digitClick={digitClicked}
                 operatorClick={executeOperation}
-                zeroClicked={clickOnZero} />
+                commaClicked={clickOnComma}
+                zeroClicked={clickOnZero}
+                changeSignum={changeSignum} />
         </div>
     )
 }
